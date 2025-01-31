4958e04213aa0cfee9767c3ecd34bf1782a8680c
ritikaraut12 <rraut1@my.centennialcollege.ca>
2025-01-31 08:49:17 -0500

    Implement feature or fix

--- obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d53e372f3c362f2f5ef7e5861983372ba8072bfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
